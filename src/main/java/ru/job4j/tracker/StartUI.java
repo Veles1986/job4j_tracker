d57a194b902d6da9b4fb7afdca9cfa2582a0c13e
Veles1986 <ilyos.rafa@gmail.com>
2024-01-25 06:39:47 +0500

    3.0. Реализация класса StartUI. Вывод меню [#500743]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -1,12 +1,43 @@
 package ru.job4j.tracker;
 
-import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
 
 public class StartUI {
+
+    public void init(Scanner scanner, Tracker tracker) {
+        boolean run = true;
+        while (run) {
+            showMenu();
+            System.out.print("Выбрать: ");
+            String selectLine = scanner.nextLine();
+            try {
+                int select = Integer.parseInt(selectLine);
+                if (select != 6) {
+                    System.out.println("Пользователь выбрал: " + select + System.lineSeparator());
+                } else {
+                    run = false;
+                }
+            } catch (NumberFormatException e) {
+                System.out.println("!!! - Неверный ввод. Введите число - !!!\n");
+            }
+        }
+    }
+
+    private void showMenu() {
+        String[] menu = {
+                "Добавить новую заявку", "Показать все заявки", "Изменить заявку",
+                "Удалить заявку", "Показать заявку по id", "Показать заявки по имени",
+                "Завершить программу"
+        };
+        System.out.println("Меню:");
+        for (int i = 0; i < menu.length; i++) {
+            System.out.println(i + ". " + menu[i]);
+        }
+    }
+
     public static void main(String[] args) {
-        Item item = new Item();
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
-        System.out.println(item.getCreated().format(formatter));
-        System.out.println(item.toString());
+        Scanner scanner = new Scanner(System.in);
+        Tracker tracker = new Tracker();
+        new StartUI().init(scanner, tracker);
     }
 }
