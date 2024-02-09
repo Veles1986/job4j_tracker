07c4f4b0be0bb94582c3e10908f6c734cb11e67d
Veles1986 <ilyos.rafa@mail.com>
2024-02-09 16:17:10 +0500

    3. final [#334073 #479686]

--- src/main/java/ru/job4j/ooa/Airbus.java
+++ src/main/java/ru/job4j/ooa/Airbus.java
@@ -23,7 +23,7 @@ public final class Airbus extends Aircraft {
     }
 
     public void printCountEngine() {
-        System.out.println(getName().equals("A380") ? 4 : COUNT_ENGINE);
+        System.out.println("A380".equals(getName()) ? 4 : COUNT_ENGINE);
     }
 
     @Override
