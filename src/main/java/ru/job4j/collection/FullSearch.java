9a5c170ec83c4ba198e8c01b6ebad02b476f1a47
Veles1986 <ilyos.rafa@mail.com>
2024-03-31 00:00:49 +0500

    1. Уникальные задачи. [#212653]

---
+++ src/main/java/ru/job4j/collection/FullSearch.java
@@ -0,0 +1,17 @@
+package ru.job4j.collection;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class FullSearch {
+
+    private Set<String> numbers = new HashSet<>();
+
+    public Set<String> extractNumber(List<Task> tasks) {
+        for (Task task : tasks) {
+            numbers.add(task.getNumber());
+        }
+        return numbers;
+    }
+}
