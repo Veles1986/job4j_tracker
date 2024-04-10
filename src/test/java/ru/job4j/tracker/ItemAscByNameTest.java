62a7eea85f5084eba904a99476006e38e1701151
Veles1986 <ilyos.rafa@mail.com>
2024-04-10 15:02:38 +0500

    0. Сортировка [#10096 #490719]

--- src/test/java/ru/job4j/tracker/ItemAscByNameTest.java
+++ src/test/java/ru/job4j/tracker/ItemAscByNameTest.java
@@ -2,7 +2,7 @@ package ru.job4j.tracker;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -10,16 +10,19 @@ import static org.assertj.core.api.Assertions.assertThat;
 class ItemAscByNameTest {
     @Test
     void whenItemAscByName() {
-        List<Item> items = new ArrayList<>();
-        items.add(new Item(342, "Sergey"));
-        items.add(new Item(453, "Bogdan"));
-        items.add(new Item(921, "Nikolay"));
-        items.add(new Item(285, "Petr"));
-        List<Item> expected = new ArrayList<>();
-        expected.add(new Item(453, "Bogdan"));
-        expected.add(new Item(921, "Nikolay"));
-        expected.add(new Item(285, "Petr"));
-        expected.add(new Item(342, "Sergey"));
+        List<Item> items = Arrays.asList(
+                new Item(342, "Sergey"),
+                new Item(453, "Bogdan"),
+                new Item(921, "Nikolay"),
+                new Item(285, "Petr")
+        );
+
+        List<Item> expected = Arrays.asList(
+                new Item(453, "Bogdan"),
+                new Item(921, "Nikolay"),
+                new Item(285, "Petr"),
+                new Item(342, "Sergey")
+        );
         Collections.sort(items, new ItemAscByName());
         assertThat(expected).isEqualTo(items);
     }
--- src/test/java/ru/job4j/tracker/ItemDescByNameTest.java
+++ src/test/java/ru/job4j/tracker/ItemDescByNameTest.java
@@ -2,7 +2,7 @@ package ru.job4j.tracker;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
@@ -11,16 +11,19 @@ import static org.assertj.core.api.Assertions.assertThat;
 class ItemDescByNameTest {
     @Test
     void whenItemAscByName() {
-        List<Item> items = new ArrayList<>();
-        items.add(new Item(342, "Sergey"));
-        items.add(new Item(453, "Bogdan"));
-        items.add(new Item(921, "Nikolay"));
-        items.add(new Item(285, "Petr"));
-        List<Item> expected = new ArrayList<>();
-        expected.add(new Item(342, "Sergey"));
-        expected.add(new Item(285, "Petr"));
-        expected.add(new Item(921, "Nikolay"));
-        expected.add(new Item(453, "Bogdan"));
+        List<Item> items = Arrays.asList(
+                new Item(342, "Sergey"),
+                new Item(453, "Bogdan"),
+                new Item(921, "Nikolay"),
+                new Item(285, "Petr")
+        );
+
+        List<Item> expected = Arrays.asList(
+                new Item(342, "Sergey"),
+                new Item(285, "Petr"),
+                new Item(921, "Nikolay"),
+                new Item(453, "Bogdan")
+        );
         Collections.sort(items, new ItemDescByName());
         assertThat(expected).isEqualTo(items);
     }
