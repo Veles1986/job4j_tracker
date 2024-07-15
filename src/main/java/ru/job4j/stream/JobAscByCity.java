edd678dcb2859123c87fd76272473ea3d1149036
Veles1986 <ilyos.rafa@mail.com>
2024-07-15 21:48:17 +0500

    7. Уникальность элементов и сортировка. [#110225 #504956]

--- src/main/java/ru/job4j/stream/JobAscByCity.java
+++
@@ -1,9 +0,0 @@
-package ru.job4j.stream;
-
-import java.util.Comparator;
-
-public class JobAscByCity implements Comparator<Address> {
-    public int compare(Address o1, Address o2) {
-        return o1.getCity().compareTo(o2.getCity());
-    }
-}
